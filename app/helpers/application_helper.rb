d6f944ce65bed8c67792984cdb967ede84043e7f
Adjoa <adjoa.darien@gmail.com>
2018-03-25 02:00:15 +0000

    li elements classed as bootstrap nav-item and nav-link in application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
   def display_sign_in
     if !current_user
-      content_tag(:li, link_to("sign in", new_user_session_path))
+      content_tag(:li, link_to("sign in", new_user_session_path, class: "nav-link"), class: "nav-item")
     end
   end
   
   def display_signup
     if !current_user
-       content_tag(:li, link_to("Sign up", new_user_registration_path))
+       content_tag(:li, link_to("Sign up", new_user_registration_path, class: "nav-link"), class: "nav-item")
     end
   end
 end
