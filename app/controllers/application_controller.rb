c548d60da1dfa0f031154bd56474795de9db5b16
Adjoa <adjoa.darien@gmail.com>
2018-03-25 13:44:54 +0000

    devise redirect for sign out overwritten in application controller; all store specs passing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  private
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    if resource_or_scope == :user
+      store_path
+    end
+  end
 end
