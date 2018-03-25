9145d31c9226dfe9c8a28509078c3f50e3841308
Adjoa <adjoa.darien@gmail.com>
2018-03-25 01:44:48 +0000

    added application helpers #display_sign_in and # display_signup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def display_sign_in
+    if !current_user
+      content_tag(:li, link_to("sign in", new_user_session_path))
+    end
+  end
+  
+  def display_signup
+    if !current_user
+       content_tag(:li, link_to("Sign up", new_user_registration_path))
+    end
+  end
 end
