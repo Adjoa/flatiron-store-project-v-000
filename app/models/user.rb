81f6d6dbe96a540f671f57f18b66f9fb297cf44c
Adjoa <adjoa.darien@gmail.com>
2018-03-25 00:44:13 +0000

    #current_cart method ideas for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,13 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :carts
+  # attr_accessor :current_cart <- Passes the spec but does not persist our cart
+  
+  # def current_cart=(current_cart) <- This may work
+  #   self.carts << current_cart
+  # end
+  
+  # def current_cart <- This may also work
+  #   Cart.where("status != ?", "submitted")
+  # end
 end
